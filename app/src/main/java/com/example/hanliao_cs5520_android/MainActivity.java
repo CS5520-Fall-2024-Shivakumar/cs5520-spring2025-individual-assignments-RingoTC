1fd2c89b9bfc618bdaf5093e88be64d7d0f719b8
ringotc <ringotc@outlook.com>
2025-01-12 09:49:20 -0800

    feat: add text view

--- app/src/main/java/com/example/hanliao_cs5520_android/MainActivity.java
+++ app/src/main/java/com/example/hanliao_cs5520_android/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hanliao_cs5520_android;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        LinearLayout layout = findViewById(R.id.main);
+
+        addText(layout, "Hello, World!");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void addText(LinearLayout layout, String text) {
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        layout.addView(textView);
     }
 }
