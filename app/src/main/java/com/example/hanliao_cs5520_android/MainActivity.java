87172cf70ded2456459985ab7ee9a89fe267f9dc
ringotc <ringotc@outlook.com>
2025-01-13 09:28:13 -0800

    feat: finish A1

--- app/src/main/java/com/example/hanliao_cs5520_android/MainActivity.java
+++ app/src/main/java/com/example/hanliao_cs5520_android/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hanliao_cs5520_android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,20 +21,19 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        LinearLayout layout = findViewById(R.id.main);
+        Button aboutMeButton = findViewById(R.id.button_id);
 
-        addText(layout, "Hello, World!");
+        aboutMeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "name: Han Liao; liao.han1@northeastern.edu", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    private void addText(LinearLayout layout, String text) {
-        TextView textView = new TextView(this);
-        textView.setText(text);
-        layout.addView(textView);
     }
 }
